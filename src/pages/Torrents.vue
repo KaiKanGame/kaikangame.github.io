67d9a488fbd236b1deacdf5832aa0d63b882a543
KaiKanGame <kaikankode@gmail.com>
2021-05-23 23:26:48 -0700

    poggie woggies

---
+++ src/pages/Torrents.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex backdrop">
+    <div class="fit column wrap justify-center items-center content-center">
+        <!-- <iframe src="https://soap2day.to/" title="Soap2Day"></iframe> -->
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
